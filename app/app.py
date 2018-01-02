31737e7da4b83607195bd138bc02bacc3ca6affc
Satoru Inoue <satoruphys@gmail.com>
2018-01-03 02:22:11 +0900

    add directory

---
+++ app/app.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,request
+from plot import plot_ticker
+
+app = Flask(__name__)
+
+app.vars = {}
+
+@app.route('/index',methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('input.html')
+    else:
+        app.vars['ticker'] = request.form['ticker']
+        
+        f = open('ticker.txt', 'w')
+        f.write(app.vars['ticker'])
+#        f.write('{ticker: \'%s\',}'%(app.vars['ticker']))
+        f.close()
+
+        plot_ticker(app.vars['ticker'])
+        
+        return render_template('output.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
