c93a41625a88b300977153c2cbea3a7356e29e59
Satoru Inoue <satoruphys@gmail.com>
2018-01-15 16:19:52 -0600

    Learning Flask

---
+++ app_old.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template,request
+from plot import plot_ticker
+
+app = Flask(__name__)
+
+app.vars = {}
+
+@app.route('/index',methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('input.html')
+    else:
+        app.vars['ticker'] = request.form['ticker']
+        
+        f = open('ticker.txt', 'w')
+        f.write(app.vars['ticker'])
+#        f.write('{ticker: \'%s\',}'%(app.vars['ticker']))
+        f.close()
+
+        #generate plot at /index/output.html
+        #This is currently not working somehow
+        plot_ticker(app.vars['ticker'])
+        
+        return render_template('output.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
