c50b4465f1baa0645f13f9e92e4b651d75a3fbee
Satoru Inoue <satoruphys@gmail.com>
2018-01-16 18:26:17 -0600

    added necessary pages

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def home():
+    """This is supposed to be the main app.
+    To be edited later."""
     return render_template('index.html')
 
 #@app.route('/user/<name>')
@@ -21,12 +23,6 @@ def about():
 def about_me():
     """This page is about me"""
     return render_template('satoru.html')
-
-@app.route('/taxi/')
-def taxi():
-    """This is supposed to be the main app.
-    To be edited later."""
-    return render_template('taxi.html')
 
 if __name__ == '__main__':
     app.run()
